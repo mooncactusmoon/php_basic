45d3caa0cabd812aa9958ad390f5c7e0200fcf7f
MoonLin <mooncactusmoon@gmail.com>
2021-10-25 15:38:53 +0800

    完成時間練習3.4

--- time/pra01.php
+++ time/pra01.php
@@ -26,10 +26,74 @@
     date_default_timezone_set('Asia/Taipei');
     $gapDays=(strtotime('2022-10-25')-strtotime('2021-10-25'))/(24*60*60);
     echo "生日還有".$gapDays."天";
+    //老師寫法
+    $now=strtotime('now');
+    $nextBirthday=strtotime('2022-10-25');
+    $gap=($nextBirthday-$now)/86400;
+    echo ceil($gap) . "天";
+    //floor()無條件捨棄到整位;ceil()無條件進位到整位;round()四捨五入
     ?>
+    <h1>利用date()函式的格式化參數，完成以下的日期格式呈現</h1>
+    <ul>
+        <li>2021/10/25</li>
+        <li>10月25日 Monday</li>
+        <li>2021-10-25 12:9:5(當下時間沒有前導0)</li>
+        <li>2021-10-25 12:09:05(當下時間有前導0)</li>
+        <li>今天是西元2021年10月25日 上班日(或假日)</li>
+    </ul>
+    <?php
+    // date_default_timezone_set('Asia/Taipei');
+    echo date("Y/m/d");
+    echo "<br>";
+    echo date('m月j日 l');
+    echo "<br>";
+    echo date("Y-n-j H:i:s");
+    echo "<br>";
+    echo date("Y-m-d H:i:s");
+    echo "<br>";
+    $date=date("今天是西元Y年m月d日");
+    if(date("N")>=6){
+        echo $date . "假日";
+    }else{
+        echo $date . "上班日";
+    }
+    ?>
+    <h1>strtotime應用</h1>
+    <h3>時間的平移</h3>
+    <?php
+    date_default_timezone_set('Asia/Taipei');
+    $now=strtotime("now");
+    echo $now;
+    echo "<br>";
+    echo date("Y-m-d H:i:s");
+    echo "<br>";
+    $yesterday=strtotime("-1 days");
+    echo "昨天".date("Y-m-d H:i:s",$yesterday);
+    echo "<br>";
+    $tomorrow=strtotime("+1 days");
+    echo "明天".date("Y-m-d H:i:s",$tomorrow);
+    echo "<br>";
+    $nextMonth=strtotime("+1 month");
+    echo "下個月".date("Y-m-d H:i:s",$nextMonth);
+    ?>
+    <h1>利用迴圈來計算連續五個周一的日期</h1>
+    <?php
+ $weekstr=['星期天','星期一','星期二','星期三','星期四','星期五','星期六',];
+ $base='2021-10-04';
+ $basetime=strtotime($base);
+ // echo date('Y-m-d',$basetime). " " .$weekstr[date("w",$basetime)];
+ 
+ for($i=0;$i<5;$i++){
+     $secs=strtotime("+".($i*7)." days",$basetime);
+     echo $i+1 . "." . " ";
+     echo date('Y-m-d',$secs). " " .$weekstr[date("w",$secs)];
+     echo "<br>";
+ }
+    ?>
+    <hr>
     <h1></h1>
-    <h1></h1>
-    <h1></h1>
+    <br>
+    <br>
 </body>
 
 </html>
