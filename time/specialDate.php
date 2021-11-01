25fb98aa38d5b941ed1ca62198398c87d6da749d
MoonLin <mooncactusmoon@gmail.com>
2021-11-01 10:55:29 +0800

    增加月曆紀念日/節日陣列示範寫法

---
+++ time/specialDate.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>月曆紀念日寫法</h1>
+    <?php
+    $specialDate=[
+        'Y-11-08'=>'日子一',
+        'Y-12-25'=>'聖誕節',
+    ];
+    echo $specialDate['Y-12-25'];
+    ?>
+</body>
+</html>
