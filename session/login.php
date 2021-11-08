33024c4d37f90853edc53cceca18e245322ea43e
MoonLin <mooncactusmoon@gmail.com>
2021-11-08 10:08:08 +0800

    初步練習cookie製作登入系統

---
+++ session/login.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>會員登入</title>
+    <style>
+        *{
+            text-align: center;
+            font-family: fantasy;
+            font-size: 18px;
+        }
+        table {
+            width: 200px;
+            margin: auto;
+            border-collapse: collapse;
+            background-color: #CCFFFF;
+        }
+        div{
+            margin: auto;
+            width: 300px;
+            height: 400px;
+            background-color: #99CCFF;
+            box-shadow:3px 3px 5px 6px #cccccc;   
+        }
+        input{
+            width: 150px;
+        }
+        .btn{
+            width: 70px;
+            background-color: #CCFFFF;
+        }
+    </style>
+</head>
+
+<body>
+    <h1>使用cookie登入並紀錄登入狀態</h1>
+    <div>
+        <?php
+        if(isset($_GET['status']) && $_GET['status']=='err'){
+            echo "<span style='color:red'>帳號或密碼錯誤</span>";
+        }
+        ?>
+        <br>
+        <form action="./login_check.php" method="POST">
+            <table border="1px">
+                <tr>
+                    <td><label for=account>Account</label></td>
+                    <td><input type="text" name="acc" id="account" placeholder="請輸入帳號"></td>
+                </tr>
+                <tr>
+                    <td><label for=password>Password</label></td>
+                    <td><input type="password" name="pw" id="password" placeholder="請輸入密碼"></td>
+                </tr>
+            </table>
+            <br>
+            <img src="../img/greeting-5480092_960_720.png" alt="歡迎圖示" width="100px" height="90px">
+            <br>
+            <input type="submit" value="Sumbit" class="btn">
+            <input type="reset" value="Cancel" class="btn">
+        </form>
+        </div>
+</body>
+</html>
---
+++ session/login_check.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>帳號密碼確認</title>
+    <style>
+        * {
+            text-align: center;
+            font-family: fantasy;
+            font-size: 30px;
+        }
+        a{
+            background-color: #CCFFFF;
+        }
+    </style>
+</head>
+<body>
+    <?php
+
+    $acc = 'moon';
+    $pw = '5566';
+        
+    if ($acc == $_POST['acc'] && $pw == $_POST['pw']) {
+        // $_SESSION['user']=$_POST['acc'];
+        setcookie('user',$_POST['acc'],time()+3600);
+        header("location:mem_center.php");
+    }else{
+        header("location:login.php?status=err");
+
+    }
+
+    ?>
+    <br>
+    <br>
+</body>
+</html>
