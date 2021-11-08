3ed094479f312b73625dec4ab1e6a927d6dda760
MoonLin <mooncactusmoon@gmail.com>
2021-11-08 09:17:48 +0800

    新增coookie完畢

---
+++ session/cookie.php
@@ -0,0 +1,8 @@
+<h1>設定cookie變數</h1>
+<p>
+變數名稱: name,值:moon,存活時間:60秒
+</p>
+<?php
+//設定cookie變數
+setcookie("name","moon",time()+60);
+?>
---
+++ session/get_cookie.php
@@ -0,0 +1,8 @@
+<?php
+session_start();
+
+echo "帳號是".$_COOKIE['name'];
+echo "<hr>";
+
+
+?>
